55fa1067ee4fe3f1912bd15c36d2a794d1fe9c50
wh1t3p1g <wh1t3p1g@gmail.com>
2022-10-26 15:29:41 +0800

    update

---
+++ run.sh
@@ -0,0 +1,9 @@
+if [ $1 = 'clean' ]
+then
+	echo "clean old data"
+	rm -rf cache/*.db && ls -alh cache | grep graphdb
+	rm -rf rules/ignores.json && ls -alh rules | grep ignores
+fi
+
+echo "start to run tabby"
+java -Xmx10g -jar build/libs/tabby-1.1.1.RELEASE.jar
